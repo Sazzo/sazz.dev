ae87b0c923a503493702c0735c3e8d7c31bc5204
Sazzo <sazz@ritsu.fun>
2021-07-28 15:35:55 -0300

    First Commit!

---
+++ components/home/Header.tsx
@@ -0,0 +1,63 @@
+import { Flex, Text, Stack, HStack } from "@chakra-ui/react";
+import { Github, Twitter } from "@styled-icons/boxicons-logos";
+import { Email } from "@styled-icons/material";
+import React from "react";
+import SazzLogo from "../common/SazzLogo";
+
+export default function Header() {
+  return (
+    <main className="header">
+      <div className="space-background"></div>
+
+      <Flex
+        paddingTop="40vh"
+        paddingLeft={{
+          base: "0rem",
+          md: "7rem",
+          xl: "7rem",
+        }}
+        flexDirection="column"
+        color="white"
+      >
+        <Stack
+          spacing={9}
+          justifyContent={{
+            base: "center",
+            md: "normal",
+            xl: "normal",
+          }}
+          alignItems={{
+            base: "center",
+            md: "normal",
+            xl: "normal",
+          }}
+        >
+          <SazzLogo width="266px" />
+          <Text
+            fontWeight="200"
+            fontStyle="normal"
+            fontSize="2xl"
+            textAlign={{
+              base: "center",
+              md: "initial",
+              xl: "initial",
+            }}
+          >
+            A kid who plays around with web development.
+          </Text>
+          <HStack spacing={19}>
+            <a href="https://twitter.com/FelipeSazz">
+              <Twitter size="35px" />
+            </a>
+            <a href="https://github.com/Sazzo">
+              <Github size="35px" />
+            </a>
+            <a href="mailto:sazz@ritsu.fun">
+              <Email size="35px" />
+            </a>
+          </HStack>
+        </Stack>
+      </Flex>
+    </main>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/overpass/300.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/main.css";
+import type { AppProps } from "next/app";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Overpass",
+    body: "Overpass",
+  },
+});
+
+function SazzWebsite({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default SazzWebsite;
