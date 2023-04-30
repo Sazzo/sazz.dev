fe38c11dcd882661c4c678b81cef219a5baac282
Sazzo <felipe@sazz.dev>
2023-04-30 15:31:26 -0300

    feat: add featured projects wip component

---
+++ src/components/ui/icons/TechnologyIcon.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import RustIcon from "./tech/RustIcon.svelte";
+  import TypeScriptIcon from "./tech/TypeScriptIcon.svelte";
+
+  export let technology: string;
+  export let className: string = "";
+
+  const technologiesIcons = [
+    {
+      name: "node",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "typescript",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "gh-actions",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "docker",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "mongodb",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "golang",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "sveltekit",
+      icon: TypeScriptIcon,
+    },
+    {
+      name: "rust",
+      icon: RustIcon,
+    },
+  ];
+  const technologyIcon = technologiesIcons.find(
+    (tech) => tech.name === technology
+  );
+</script>
+
+<svelte:component this={technologyIcon.icon} class={className} />
