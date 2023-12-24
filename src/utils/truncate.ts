e10c2bd3649632613f5edcb4725fb89993ec5aa8
Sazzo <felipe@sazz.dev>
2023-12-24 19:31:02 -0300

    refactor: cleanup

--- src/utils/truncate.ts
+++
@@ -1,3 +0,0 @@
-// https://github.com/ven/ven-earth/blob/master/src/lib/utils.ts
-export const truncate = (str: string, n: number) =>
-  str.length > n ? str.substr(0, n - 1) + "..." : str;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        syne: ["Syne", "sans-serif"],
-      },
-
       padding: {
         nav: "6rem",
       },
